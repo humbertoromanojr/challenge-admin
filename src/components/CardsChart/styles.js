d659053db9aced2fe32ce82ca4a13d4a01edf89e
JR Dev <astronomi@gmail.com>
2020-02-11 01:56:59 -0300

    Update styles.js

--- src/components/CardsChart/styles.js
+++ src/components/CardsChart/styles.js
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: block;
-  margin: 0px 0px 12px 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 12px 12px 12px;
   max-width: 322px;
-  height: 18%;
+  height: 19%;
   font-family: Muli;
   font-style: normal;
   font-weight: bold;
   font-size: 19px;
-  line-height: 24px;
 
   text-align: center;
   letter-spacing: 0.4px;
@@ -19,9 +21,8 @@ export const Title = styled.span`
   font-family: Muli;
   font-style: normal;
   font-weight: 600;
-  font-size: 16px;
-  line-height: 22px;
-  margin-bottom: 10px;
+  font-size: 14px;
+  line-height: 20px;
 
   text-align: center;
   letter-spacing: 0.3px;
@@ -39,6 +40,5 @@ export const Information = styled.h3`
   line-height: 30px;
   text-align: center;
   letter-spacing: 0.3px;
-
   color: #252733;
 `;
