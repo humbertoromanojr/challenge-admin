b935bc49c0dcafc3311af77193052c7243319598
JR Dev <astronomi@gmail.com>
2020-02-11 01:56:47 -0300

    Update styles.js

--- src/pages/Overview/styles.js
+++ src/pages/Overview/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Muli:400,600&display=swap');
   display: flex;
   margin: 40px 0px 0px 0px;
 `;
@@ -20,8 +21,7 @@ export const ContainerChart = styled.section`
   flex-direction: row;
   justify-content: space-between;
   margin: 0px 0px 30px 0px;
-  padding: 20px 20px 20px 20px;
-  max-width: 1132px;
+  max-width: 1122px;
 
   background: #FFFFFF;
   border: 1px solid #DFE0EB;
@@ -29,7 +29,7 @@ export const ContainerChart = styled.section`
 
   & header {
     display: block;
-    margin-bottom: 20px;
+    padding: 20px 0px 0px 20px;
 
     & span {
       font-family: Muli;
@@ -45,10 +45,12 @@ export const ContainerChart = styled.section`
   & article {
     & div {
       border-bottom: 1px solid #DFE0EB;
+      border-left: 1px solid #DFE0EB;
+      border-collapse: collapse;
     }
 
     & div:last-child {
-      border: 1px solid #fff;
+      border-bottom: 1px solid #fff;
     }
   }
 `;
@@ -56,4 +58,28 @@ export const ContainerChart = styled.section`
 export const ContainerCardsFooter = styled.section`
   display: flex;
   flex-direction: row;
+`;
+
+export const CreateNewTask = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  & h3 {
+    font-family: Muli;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 20px;
+    padding-left: 20px;
+    letter-spacing: 0.2px;
+    color: #C5C7CD;
+  }
+
+  & button {
+    background: #F0F1F7;
+    border-radius: 8px;
+    max-width: 30px;
+    margin-right: 20px;
+  }
 `;
