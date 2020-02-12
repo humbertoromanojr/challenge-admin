a887cf02b33e9c6709098ce3dbb03ec0dff4b952
JR Dev <astronomi@gmail.com>
2020-02-11 23:54:09 -0300

    Update index.js and styles.js

--- src/pages/Tickets/index.js
+++ src/pages/Tickets/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { FaFilter, FaSortAmountUp } from 'react-icons/fa';
+
+import {
+  Container,
+  Content,
+  ContentHeader,
+} from './styles';
 
 export default function Tickets() {
   return (
     <Container>
-      Tickets page
+      <Content>
+        <ContentHeader>
+          <h3>All tickets</h3>
+          <div>
+            <span>
+              <FaSortAmountUp
+                size={12}
+                color="#ddd"
+                style={{ marginRight: 10 }}
+              />
+              Sort
+            </span>
+            <span>
+              <FaFilter
+                size={10}
+                color='#ddd'
+                style={{ marginRight: 10 }}
+              />
+              Filter
+            </span>
+          </div>
+        </ContentHeader>
+      </Content>
     </Container>
   );
 }
--- src/pages/Tickets/styles.js
+++ src/pages/Tickets/styles.js
@@ -2,14 +2,36 @@ import styled from "styled-components";
 
 export const Container = styled.header`
   display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px 20px;
+  margin: 40px 0px 0px 0px;
 `;
 
 export const Content = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px 0px 0;
+  flex: 1;
+  background: #fff;
+  min-width: 1122px;
 `;
+
+export const ContentHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  width: 100%;
+
+  & span {
+    align-items: center;
+    padding: 0px 30px 0px 5px;
+
+    font-family: Muli;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: 0.2px;
+    color: #4B506D;
+
+  }
+`;
+
