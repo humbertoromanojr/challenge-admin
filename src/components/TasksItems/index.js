3b7f45e39ff96e85f5a1e173cf8972217b138a09
JR Dev <astronomi@gmail.com>
2020-02-11 22:21:48 -0300

    Update index.js

--- src/components/TasksItems/index.js
+++ src/components/TasksItems/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { FaCheckCircle } from 'react-icons/fa';
 import Button from '@material-ui/core/Button';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 
 import { Container, TitleText } from './styles';
 
 const TasksItems = props => (
   <Container>
     <div>
-      <FaCheckCircle
-        size={20}
-        color='#3751FF'
-      />
+      {
+        props.check ? (<FaCheckCircle
+          size={20}
+          color='#3751FF'
+        />) : (
+          <RadioButtonUncheckedIcon
+            size={20}
+            color='#ddd'
+            fontSize='medium'
+          />
+        )
+      }
       <TitleText>{props.title}</TitleText>
     </div>
     <Button variant="contained" color={props.typeButton}>{props.textButton}</Button>
